44b1b1d1db2c0bd71ac63facf1cb59fb4145b868
Narrabeen010 <60920688+Narrabeen010@users.noreply.github.com>
2020-05-03 21:41:23 +1000

    restored to base

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,35 @@
-import { useState } from "react";
-import Router from "next/router";
+import React from 'react'
+import Head from 'next/head'
+import Logo from '../components/logo'
 
-import Layout from "../components/Layout";
-import Row from "../components/prebuilt/Row";
-import DonutShop from "../components/prebuilt/DonutShop";
-import CheckoutForm from "../components/CheckoutForm";
-import getDonutPrice from "../utils/get-donut-price";
+export default () => (
+  <div className="root">
+    <Head>
+      <meta charSet="utf-8"/>
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <title>Next.js on Heroku</title>
+    </Head>
+    <style jsx>{`
+      .root {
+        font-family: sans-serif;
+        line-height: 1.33rem;
+        margin-top: 8vh,
+      }
+      @media (min-width: 600px) {
+        .root {
+          margin-left: 21vw;
+          margin-right: 21vw;
+        }
+      }
+    `}</style>
 
-const MainPage = props => {
-  const [numDonuts, setNumDonuts] = useState(1);
+    <h1><Logo style={{ height: '1.45rem' }}/> Next.js on Heroku</h1>
 
-  const addDonut = () => setNumDonuts(num => Math.min(12, num + 1));
-  const remDonut = () => setNumDonuts(num => Math.max(1, num - 1));
+    <p>Deploy <a href="https://nextjs.org/">Next.js</a> universal web apps on <a href="https://www.heroku.com/home">Heroku</a>.</p>
 
-  return (
-    <Layout title="Donut Shop">
-      <Row>
-        <DonutShop
-          onAddDonut={addDonut}
-          onRemoveDonut={remDonut}
-          numDonuts={numDonuts}
-        />
-      </Row>
-      <CheckoutForm
-        price={getDonutPrice(numDonuts)}
-        onSuccessfulCheckout={() => Router.push("/success")}
-      />
-    </Layout>
-  );
-};
+    <p>This <strong>demo deployment on Heroku</strong> is from the repo <a href="https://github.com/mars/heroku-nextjs">mars/heroku-nextjs</a>.</p>
 
-export default MainPage;
+    <p><a href="https://github.com/mars/heroku-nextjs/archive/master.zip">Download this Next.js app</a> as a Heroku-ready template, or follow <a href="https://github.com/mars/heroku-nextjs#production-deployment">Production Deployment</a> to push an existing app to Heroku.</p>
+  </div>
+)
